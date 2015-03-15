7a8689e9d07d6334ce5462e25275c980dbbf3575
Angel Marino <me@angelmarino.com>
2015-03-15 17:31:24 -0400

    Basic routing for /, /404, and /500 routes

--- program.js
+++ program.js
@@ -1,8 +1,19 @@
 var koa = require('koa');
 var app = koa();
 
-app.use(function *(){
-	this.body = 'hello koa';
+app.use(function* (next){
+	if (this.path !== '/') return yield next;
+		this.body = 'hello koa';		
+});
+
+app.use(function* (next){
+	if (this.path !== '/404') return yield next;
+		this.body = 'page not found';		
+});
+
+app.use(function* (next){
+	if (this.path !== '/500') return yield next;
+		this.body = 'internal server error';		
 });
 
 var port = process.argv[2];
